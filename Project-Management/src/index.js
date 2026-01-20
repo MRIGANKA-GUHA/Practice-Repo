dfd590bd08a43dba67664e1e0963b3a2dd560321
MRIGANKA-GUHA <mrigankaguha42@gmail.com>
2026-01-20 21:23:13 +0530

    Adding Database connection

--- Project-Management/src/index.js
+++ Project-Management/src/index.js
@@ -1,11 +1,17 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/database-connection.js"
 dotenv.config({
     path: "./.env",
 });
 
 const port = process.env.PORT || 6969;
 
-app.listen(port, () => {
-    console.log(`App listening on port http://localhost:${port}`);
-});
+connectDB()
+.then(() => {
+    app.listen(port, () => {
+    console.log(`App listening on port http://localhost:${port}`);});
+})
+.catch((err)=> {
+    console.log("MONGODB connection error", err);
+})
