bde7e3b07188cc3a4e004b5dddc67f1852f43624
MRIGANKA-GUHA <mrigankaguha42@gmail.com>
2026-01-22 23:09:01 +0530

    Adding Async Handler

--- Project-Management/src/controllers/healthcheck-controller.js
+++ Project-Management/src/controllers/healthcheck-controller.js
@@ -1,18 +1,13 @@
 import { apiResponse } from "../utils/api-response.js";
-import { apiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
-const healthCheck = (req, res) => {
-    try {
-        res.status(200) //it tells the browser everything is ok
-            .json(
-                //it basically sends the response to the user
-                new apiResponse(200, { message: "Server is Running" }),
-            );
-    } catch (error) {
-        res.status(500).json(
-            new apiError(500, { message: "Something wrong with Healthcheck" }),
+const healthCheck = asyncHandler(
+    async (req, res) => {
+    res.status(200) //it tells the browser everything is ok
+        .json(                                                       //it basically sends the response to the user
+            new apiResponse(200, { message: "Server is Running" }),
         );
-    }
-};
+}
+)
 
 export { healthCheck };
