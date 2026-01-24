428b52969bb8826bdb804b9bb28e788e45640343
MRIGANKA-GUHA <mrigankaguha42@gmail.com>
2026-01-24 21:11:40 +0530

    auth-middleware

---
+++ Project-Management/src/middlewares/auth-middleware.js
@@ -0,0 +1,29 @@
+import { User } from "../models/user-model.js";
+import { apiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+
+const verifyJWT = asyncHandler(async (req, res, next) => {
+    const token =
+        req.cookies?.accessToken ||
+        req.header("Authorization")?.replace("Bearer ", ""); //Bearer is used for mobile Application
+
+    if (!token) {
+        throw new apiError(401, "Unauthorized request");
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const user = await User.findById(decodedToken?._id).select(
+            "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+        );
+
+        if (!user) {
+            throw new apiError(401, "Invalid access token");
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {}
+});
+
+export { verifyJWT };
